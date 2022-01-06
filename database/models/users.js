42172f7aab8bfc650ec6d36a44d5670bb8bb1570
Dennis van den Berg <dennispvandenberg@gmail.com>
2022-01-06 17:47:56 +0100

    Add models to db

---
+++ database/models/users.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('users', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    provider: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    provider_uid: {
+      type: DataTypes.STRING(2048),
+      allowNull: true
+    },
+    first_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    hash: {
+      type: DataTypes.STRING(2048),
+      allowNull: true
+    },
+    salt: {
+      type: DataTypes.STRING(2048),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'users',
+    timestamps: true,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
