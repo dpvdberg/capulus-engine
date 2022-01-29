7a25957fad943f8249b21faff30f72a35faf9e86
Dennis van den Berg <dennispvandenberg@gmail.com>
2022-01-29 01:35:56 +0100

    Fix google callback url

--- routes/api/auth/strategies/google.js
+++ routes/api/auth/strategies/google.js
@@ -8,7 +8,7 @@ passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         scope: ['profile', 'email'],
-        callbackURL: `/api/auth/google/callback`
+        callbackURL: `${process.env.APP_URL}/api/auth/google/callback`
     },
     function (accessToken, refreshToken, profile, cb) {
         const issuer = 'google'
