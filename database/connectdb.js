c0d3521129b75a212c5f6883eca2d2bc9d552b1e
Dennis van den Berg <dennispvandenberg@gmail.com>
2022-01-31 01:13:17 +0100

    WIP trending page

--- database/connectdb.js
+++ database/connectdb.js
@@ -1,9 +1,11 @@
 const {Sequelize} = require("sequelize");
 console.log("Using db user: " + process.env.MYSQL_USER)
+console.log(process.env.NODE_ENV)
 const sequelize = new Sequelize(process.env.MYSQL_USER, process.env.MYSQL_DB, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
     dialectOptions: {
+        decimalNumbers: true,
         dateStrings: true,
         typeCast: true,
         timezone: "local",
--- routes/api/data/module/stats.js
+++ routes/api/data/module/stats.js
@@ -3,7 +3,7 @@ const {models} = require("../../../../database/connectmodels");
 const sequelize = require("sequelize");
 const router = express.Router();
 
-router.get('/calendar/orders', (req, res) => {
+router.get('/orders/calendar', (req, res) => {
     models.orders.findAll({
         attributes: [
             [ sequelize.fn('date_format', sequelize.col('createdAt'), '%Y-%m-%d'), 'day'],
@@ -11,6 +11,23 @@ router.get('/calendar/orders', (req, res) => {
         ],
         group: 'day',
         order: [[sequelize.col('day'), 'DESC']]
+    }).then((orders) => {
+        res.json(orders)
+    })
+})
+
+router.get('/orders/products', (req, res) => {
+    models.order_products.findAll({
+        attributes: [
+            [ sequelize.fn('sum', sequelize.col('quantity')), 'value'],
+            [ sequelize.col('product.name'), 'name']
+        ],
+        include: {
+            model: models.products,
+            attributes : [],
+        },
+        group: 'product_id',
+        order: [[sequelize.col('value'), 'DESC']]
     }).then((orders) => {
         res.json(orders)
     })
