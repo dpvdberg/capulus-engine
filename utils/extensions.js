ebe06c6f4c2399869b89841e5e2f6b458a48f5d2
Dennis van den Berg <dennispvandenberg@gmail.com>
2022-02-02 17:06:04 +0100

    Fix timezone

--- utils/extensions.js
+++ utils/extensions.js
@@ -5,7 +5,7 @@ Date.createFromMysql = function (mysql_string) {
         t = mysql_string.split(/[- :]/);
 
         //when t[3], t[4] and t[5] are missing they defaults to zero
-        result = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0));
+        result = new Date(t[0], t[1] - 1, t[2], t[3] || 0, t[4] || 0, t[5] || 0);
     }
 
     return result;
